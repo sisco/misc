c675cf7cbef70f1348ce503544b1e0f3abfb08ab
Brian Sisco <bartolo42@gmail.com>
2012-08-28 22:23:43 -0400

    Added function that scrapes webpage for links to and downloads fare data files.

--- Transit/transit.py
+++ Transit/transit.py
@@ -9,14 +9,17 @@ def main():
     """Performs various sorts of transit data analysis.
     Options:
     help    This info.
-    q       Or quit.
+    q       Quit.
     init    Drop/add tables, populate with data from existing files.
-    d       Download turnstile data files form MTA.info
+    init f  Above, but for fare data instead of turnstile data.
+    d       Download turnstile data files from MTA.info
+    d f     Download fare data files from MTA.info
     %       Show stations with x% more entries than exits.
     /       Show stations with the highest ratio of entrances/exits.
     +       Show stations with the greatest difference between entrances and exits.
     crowded Show stations with the greatest entries per turnstile.
     annual  Calculate annual totals for 2011 (only full year with data).
+    common  Show stations in common between lists of stations with most entries and most entries/turnstile
     
     Any other input is interpreted as an SQL command with results displayed.
     
@@ -29,18 +32,24 @@ def main():
         x = raw_input("Input: ")
         if x == "help" or x == "h":
             print main.__doc__
+        elif x == "quit" or x == "q":
+            return "Goodbye"
+        elif x == "pass":
+            continue
         elif x == "init":
             init_db(conn)
+        elif x == "init f":
+            init_fare(conn)
         elif x == "download" or x == "d":
             input = raw_input("Download all files or new files? (all/new) ")
             if input == "all" or input == "a":
                 download_turnstile_files()
             else:
                 download_new_turnstile_files()
-        elif x == "quit" or x == "q":
-            return "Goodbye"
-        elif x == "pass":
-            continue
+        elif x == "download fare" or x == "d fare" or x == "d f":
+            #Download a separate dataset from the MTA.
+            download_fare_files()
+        
         elif x == "%":
             #Show stations with x% more entries than exits or between x% and y% more entries than exits.
             x = raw_input("Enter min (or min and max) % increase of entries over exits: ")
@@ -108,7 +117,18 @@ def main():
                 for row in rows:
                     print [str(i) for i in row if not is_number(i)] + [i for i in row if is_number(i)]
                 print 'Needed to compare the Top {0} Lists to find {1} stations in common.'.format(num, goal)
-                   
+        elif x == "year sum":
+            #System wide totals for 2011.
+            print "System wide totals for 2011."
+            x = '''SELECT sum(entries), sum(exits), sum(entries) - sum(exits)
+                    FROM annual_totals_by_name'''
+            print "Entries, exits"
+            print_all_rows(c, x)
+            
+            x = '''SELECT sum(entries), sum(exits), sum(entries) - sum(exits)
+                    FROM annual_totals'''
+            print "Entries, exits"
+            print_all_rows(c, x)
         elif x == "b":
             #b for builtin, execute whatever line is put here
             
@@ -184,6 +204,7 @@ def init_db(conn):
     import glob
     for file in glob.glob("data/turnstile_*.txt"):
         parse_turnstile_file(file, c)
+        conn.commit()
         
     try:            
         c.execute('''DROP TABLE turnstile_totals''')
@@ -236,6 +257,25 @@ def init_db(conn):
     # We can also close the cursor if we are done with it
     c.close()
 
+def init_fare(conn):
+    #Create the tables that are based on fare data.
+    
+    #Cursors made from the connection are used to execute commands.
+    c = conn.cursor()
+    
+    try:
+        c.execute('''DROP TABLE raw_fare_data''')
+    except sqlite3.OperationalError:
+        print "raw_fare_data could not be dropped"
+    c.execute('''CREATE TABLE raw_fare_data
+                 (REMOTE text, STATION text, FF integer,SEN/DIS integer, 7-D AFAS UNL integer,30-D AFAS/RMF UNL integer,JOINT RR TKT integer,7-D UNL integer,30-D UNL integer,14-D RFM UNL integer,1-D UNL integer,14-D UNL integer,7D-XBUS PASS integer,TCMC integer,LIB SPEC SEN integer,RR UNL NO TRADE integer,TCMC ANNUAL MC integer,MR EZPAY EXP integer,MR EZPAY UNL integer,PATH 2-T integer,AIRTRAIN FF integer,AIRTRAIN 30-D integer,AIRTRAIN 10-T integer,AIRTRAIN MTHLY integer, STUDENTS integer)''')
+    
+    #Iterate over each datafile, adding the info to the db.
+    import glob
+    for file in glob.glob("fares/fares_*.csv"):
+        parse_fare_file(file, c)
+        conn.commit()
+    
 def is_number(s):
     try:
         float(s)
@@ -335,6 +375,39 @@ def download_new_turnstile_files():
                     links.write(url)
                     print "Downloaded:" + url
                 
+def download_fare_files():
+    #Download the fare data distributed by the MTA at http://www.mta.info/developers/fare.html
+    #This downloads ~115 files, each 170 kb, so don't call it more than once.
+    
+    from lxml import etree
+    parser = etree.HTMLParser()
+    tree = etree.parse("http://www.mta.info/developers/fare.html", parser)
+    
+    #The list the urls will go in.
+    urls = []
+    #Uses an XPATH expression to find the div that contains the list of data files.
+    print "Parsing http://www.mta.info/developers/fare.html"
+    r = tree.find(".//div[@class='span-19 last']")
+    #for each element in the div, if it is an <a>, we put the url in the list
+    for child in r:
+        if child.tag == 'a':
+            urls.append(child.get('href'))
+    print "Page parsed. {0} links found.".format(len(urls))
+    
+    #The following actually downloads the files. 
+    import urllib2        
+    for url in urls:
+        f = urllib2.urlopen("http://www.mta.info/developers/" + url)
+        with open("fares/" + url.split('/')[-1], "wb") as code:
+            code.write(f.read())
+        print "Downloaded {0}".format(url)
+    with open("fares/links.txt", "a") as f:
+        for url in urls:
+            f.write("http://www.mta.info/developers/" + url + '\n')
+    print "Download complete."
+
+
+    
 def construct_year_totals(conn):
     #Given the database connections, create a table of annual totals for 2011, the only full year with data.
     c = conn.cursor()
